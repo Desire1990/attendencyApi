99c678fc9ad41b95cd79bae8820ac75631b80112
Desire1990 <desnduwim26@gmail.com>
2023-07-05 18:46:07 +0200

    buja timezone added

--- attendencyApi/settings.py
+++ attendencyApi/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Bujumbura'
 
 USE_I18N = True
 
@@ -139,7 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
 }
 
 SIMPLE_JWT = {
